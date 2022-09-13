14ed4867d726585a15cbf4dad31fdc7c9a7e0d99
Kyle <ky42@protonmail.com>
2022-09-12 21:46:49 -0230

    Worked on navbar

--- frontend/src/components/NavBar/DesktopNavBar.tsx
+++ frontend/src/components/NavBar/DesktopNavBar.tsx
@@ -7,14 +7,18 @@ const NavBarDiv = styled.nav`
   width: 100%;
   position: fixed;
   bottom: 0%;
+  display: flex;
+  justify-content: space-around;
 `;
 
 const NavBarLink = styled(Link)`
+  text-align: center;
+  flex-grow: 5;
 
 `;
 
 const HomeLink = styled(NavBarLink)`
-
+  flex-grow: 2;
 `;
 
 const DesktopNavBar = () => {
