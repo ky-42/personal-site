52672f1a8d01b69710d0d3506ae51af4fe9e86ec
Kyle <ky42@protonmail.com>
2022-11-04 17:31:27 -0230

    db migrations in progress

--- backend/migrations/2022-11-03-144723_enforce_content_relationship/up.sql
+++ backend/migrations/2022-11-03-144723_enforce_content_relationship/up.sql
@@ -3,10 +3,11 @@
 -- the content type on the base content row
 UPDATE project SET id = content_id;
 ALTER TABLE project ADD COLUMN content_type TEXT DEFAULT 'project' CHECK (content_type = 'project');
-ALTER TABLE project ADD FOREIN KEY (id, content_type) REFERENCES content (id, content_type);
+ALTER TABLE project ADD FOREIGN KEY (id) REFERENCES content (id);
+-- ALTER TABLE project ADD FOREIGN KEY (content_type) REFERENCES content (content_type);
 ALTER TABLE project DROP COLUMN content_id;
 
-UPDATE blog SET id = content_id;
-ALTER TABLE blog ADD COLUMN content_type TEXT DEFAULT 'blog' CHECK (content_type = 'blog');
-ALTER TABLE blog ADD FOREIN KEY (id, content_type) REFERENCES content (id, content_type);
-ALTER TABLE blog DROP COLUMN content_id;
+-- UPDATE blog SET id = content_id;
+-- ALTER TABLE blog ADD COLUMN content_type TEXT DEFAULT 'blog' CHECK (content_type = 'blog');
+-- ALTER TABLE blog ADD FOREIGN KEY (id, content_type) REFERENCES content (id, content_type);
+-- ALTER TABLE blog DROP COLUMN content_id;
