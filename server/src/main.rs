eed87fb79cab28c642c1f4e8d8389e083c8de7fd
Kyle <ky42@protonmail.com>
2025-09-21 13:00:32 -0230

    Change server binding address to allow external access

--- server/src/main.rs
+++ server/src/main.rs
@@ -34,7 +34,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(web::Data::new(admin_info.clone()))
             .configure(configs::route_config)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
